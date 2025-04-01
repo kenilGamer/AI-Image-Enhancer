71bbbea90cc2d237956dc7de1e33d2bba74b7198
kenil Gamer <kenilk677@gmail.com>
2025-04-01 16:51:30 +0530

    sdfsd

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Home from './Home'
+import { Link } from 'react-router-dom'
+
 
 function MainPage() {
   return (
     <div className="flex items-center justify-center h-screen flex-col bg-gray-200 py-8 px-4">
-    <header className="text-center mb-8 p-4">
+    <div className="text-center mb-8 p-4">
       <h1 className="text-4xl font-bold mb-2">AI Image Enhancer</h1>
       <p className="text-lg text-gray-600">Upload an image and get a better one</p>
-    </header>
+    </div>
     <main>
       <Home />
     </main>
     <footer className="text-lg text-gray-600 text-center mt-3">
       <p>Powered by <span className="font-semibold">@Godcraft</span></p>
+      <Link 
+        to="/ai-gn" 
+        className="inline-block bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-600 hover:shadow-lg transition-all duration-300"
+      >
+        Go to AI Logo Generator
+      </Link>
     </footer>
   </div>
   )
